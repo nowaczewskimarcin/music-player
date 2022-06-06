29012a946b79f8ed29cf4fa4d373b0ffc68fa7e6
Marcin Nowaczewski <nowaczewskimarcin@gmail.com>
2022-06-06 18:51:08 +0200

    add buttons to player & add el:'#playerComponent',

--- music-player/src/components/PlayerComponent.vue
+++ music-player/src/components/PlayerComponent.vue
@@ -1,24 +1,57 @@
 <template>
+<section id="playerComponent">
   <p>player controls</p>
 
-      <div class="text-purple q-gutter-md" style="font-size: 2em">
-  <q-icon name="style" @click="firstFunction()" /> 
+      <div class="text-purple q-gutter-md row justify-center items-center" style="font-size: 2em">
+  <div><text-body1>ssss </text-body1></div>
+  <div>
+  <q-btn class="btn-player previous"  @click="previousSong()" icon="fa-solid fa-backward-step" /> 
+  <q-btn class="btn-player play"  v-if="!isPlaying" @click="playSong()" icon="fa-solid fa-play" />
+  <q-btn class="btn-player pause" v-else @click="pauseSong()" icon="fa-solid fa-pause" />
+  <q-btn class="btn-player forward" @click="nextSong()" icon="fa-solid fa-forward-step" />
+  </div>
     </div>
-
+ </section>
 </template>
  
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
+  el: '#playerComponent',
   name: 'PlayerControls',
-
   components: {},
+  data() {
+        return ({
+     isPlaying: false,
+    
+    
+        })
+    },
+  
   computed: {},
   methods: {
-      firstFunction() {
-          console.log('function call');
+      previousSong() {
+          console.log('User changed previous song <<<');
+      },
+      playSong() {
+        this.isPlaying = true;
+          console.log('Song is playing');
+      },
+      pauseSong() {
+        this.isPlaying = false;
+          console.log('Song is pausing');
+      },
+      nextSong() {
+          console.log('User changed forward song >>>');
       }
   }
 });
 </script>
+
+
+<style scoped>
+.btn-player {
+
+}
+</style>
